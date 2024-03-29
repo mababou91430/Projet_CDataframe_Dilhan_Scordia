fe04554a5673064266bc9ed64f0203918244133f
mathm <mathieu.dilhan91@gmail.Com>
2024-03-29 12:12:05 +0100

    add main

--- fonction.c
+++ fonction.c
@@ -1,1 +1,14 @@
 #include "fonction.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+COLUMN *create_column(char *title){
+    int *tab[256];
+    COLUMN * column = (COLUMN*)malloc(sizeof (COLUMN));
+    column->titre=title;
+    column->tab = *tab;
+    column->taille_Logique=256;
+    column->taille_Physique=256;
+    return column;
+}
