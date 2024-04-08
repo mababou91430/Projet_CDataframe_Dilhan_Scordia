ac055f04c3eca03e9446ceececb270ebaa3c10b4
mathm <mathieu.dilhan91@gmail.Com>
2024-04-08 16:24:26 +0200

    Tous le 4

--- main.c
+++ main.c
@@ -3,15 +3,14 @@
 #include <stdlib.h>
 
 int main() {
+    int x;
     CDataframe* Dataframe = creation_CDataframe();
     for(int i=0;i<3;i++){
         ajout_ligne_CDataFrame(Dataframe);
     }
     ajout_colonne_CDataFrame(Dataframe, "colonne ajouté");
     ajout_colonne_CDataFrame(Dataframe, "colonne ajouté2");
-    remplissage_CDataframe(Dataframe);
-    afficher_All_CDataframe(Dataframe);
-    printf("Taille logique %d\n ",Dataframe->tab[0].taille_Logique);
-    printf("case contenant 0 %d", nombre_cellules_contenant_X(Dataframe,0));
+    remplissage_Dur_CDataframe(Dataframe);
+    printf("Nombre de 0 : %d", nombre_cellules_inferieur_X(Dataframe,0));
     return 0;
 }
